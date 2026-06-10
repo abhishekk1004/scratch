119f11734fe636027efb161df13d40f650107dfc
abhishekk1004 <abhishekkushwaha.np@gmail.com>
2026-06-10 12:31:37 +0545

    From Scratch

---
+++ lib/constants/colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// Primary Branding Colors
+const Color black=Color(0x0fffffff);
+const Color kPrimary = Color(0xFF6200EA);
+const Color kPrimaryLight = Color(0xFF9E47FF);
+const Color kPrimaryDark = Color(0xFF0A00B6);
+
+// Secondary/Accent Colors
+const Color kAccent = Color(0xFF03DAC6);
+
+// Background and Surface Colors
+const Color kBackground = Color(0xFFF5F5F5);
+const Color kSurface = Color(0xFFFFFFFF);
+
+// Status Colors
+const Color kError = Color(0xFFB00020);
+const Color kSuccess = Color(0xFF4CAF50);
+const Color kWarning = Color(0xFFFFC107);
+
+// Typography/Text Colors
+const Color kTextPrimary = Color(0xFF212121);
+const Color kTextSecondary = Color(0xFF757575);
